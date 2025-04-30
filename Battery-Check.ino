0ac9814c92e731638b18bc053832adc1262c4073
okm <64970074+plexede@users.noreply.github.com>
2025-04-30 18:12:09 -0400

    styling
    Signed-off-by: okm <64970074+plexede@users.noreply.github.com>

--- Battery-Check.ino
+++ Battery-Check.ino
@@ -2,20 +2,19 @@
     Ensure the battery is suitably charged before operation
 */
 
-void BatteryCheck()
-{
-    /*
+void BatteryCheck() {
+  /*
     pseudo code:
     read battery voltage:
         if below 11 volts, turn on red light
         if 900 volts or below prizm.PrizmEnd();
     */
-    int batteryVoltage = prizm.readBatteryVoltage();
-    
-    if(batteryVoltage < 900) {
-      prizm.PrizmEnd();
-    }
-    if(batteryVoltage <= 1100) {
-      prizm.setRedLED(HIGH);
-    }
+  int batteryVoltage = prizm.readBatteryVoltage();
+
+  if (batteryVoltage < 900) {
+    prizm.PrizmEnd();
+  }
+  if (batteryVoltage <= 1100) {
+    prizm.setRedLED(HIGH);
+  }
 }
