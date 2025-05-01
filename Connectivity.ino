613d63d1d0be53f548e77ae719ddd3c97f329cdd
mackereal <malikelhabbak@gmail.com>
2025-05-01 14:35:49 -0400

    Controller Vibrate
    Controller vibrates if not in range now
    (a profession did this btw)

--- Connectivity.ino
+++ Connectivity.ino
@@ -1,8 +1,24 @@
 void Connectivity() {
   /*
-  if connected, do dance
+  if connected, do nothing
   else prizm.PrizmEnd();
 */
-  {
-    prizm.prizmEnd();
+  if (!ps4.Connected) {
+    prizm.PrizmEnd();
   }
+
+  Range();
+}
+
+/*
+  if out of connectivity range, vibrate controller
+  */
+void Range() {
+  ps4.inRange;
+  if (!ps4.inRange) {
+   ps4.setRumble(FAST);
+  } 
+  else {
+    ps4.setRumble(STOP);
+  }
+}
