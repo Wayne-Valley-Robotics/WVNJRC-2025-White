8794857be638b2daf56203d9656deda5a71862d6
aayush-pandya <ahpandya06@gmail.com>
2025-05-03 14:38:45 -0400

    flipped lx and rx
    frgiuefwdefvedsfogjufvdhjufghj

--- Drive.ino
+++ Drive.ino
@@ -5,9 +5,9 @@
 const int TORQUENADO_MAX_SPEED = 630; // max speed for torquenado
 const int MOTORS_MAX_SPEED = 720;     // max physical speed, effectively disabling torquenado
 
-int JOY_LX;
-int JOY_LY;
 int JOY_RX;
+int JOY_LY;
+int JOY_LX;
 int JOY_RY;
 
 int lf_val;
@@ -31,25 +31,25 @@ void DriveSystem()
   // plz work
   // i dont want to redo this
 
-  JOY_LX = map(ps4.Stick(LX), 0, 255, -100, 100);
-  JOY_LX = JOY_LX * (abs(JOY_LX) > deadzone);
+  JOY_RX = map(ps4.Stick(LX), 0, 255, -100, 100);
+  JOY_RX = JOY_RX * (abs(JOY_RX) > deadzone);
   JOY_LY = map(ps4.Stick(LY), 0, 255, -100, 100);
   JOY_LY = JOY_LY * (abs(JOY_LY) > deadzone);
 
-  JOY_RX = map(ps4.Stick(RX), 0, 255, -100, 100);
-  JOY_RX = JOY_RX * (abs(JOY_RX) > deadzone);
+  JOY_LX = map(ps4.Stick(RX), 0, 255, -100, 100);
+  JOY_LX = JOY_LX * (abs(JOY_LX) > deadzone);
 
-  lf_val = constrain(JOY_LY + JOY_LX + JOY_RX, -range, range);
-  rf_val = constrain(JOY_LY - JOY_LX - JOY_RX, -range, range);
-  lb_val = constrain(JOY_LY - JOY_LX + JOY_RX, -range, range);
-  rb_val = constrain(JOY_LY + JOY_LX - JOY_RX, -range, range); 
+  lf_val = constrain(JOY_LY + JOY_RX + JOY_LX, -range, range);
+  rf_val = constrain(JOY_LY - JOY_RX - JOY_LX, -range, range);
+  lb_val = constrain(JOY_LY - JOY_RX + JOY_LX, -range, range);
+  rb_val = constrain(JOY_LY + JOY_RX - JOY_LX, -range, range); 
 
   /*
   2, 2 br
   fr: 1, 2
   */
   exc.setMotorPower(1, 1, -lf_val);
-  exc.setMotorPower(2, 1, lb_val);
+  exc.setMotorPower(2, 1, -lb_val);
   exc.setMotorPower(1, 2, rf_val);
   exc.setMotorPower(2, 2, rb_val);
 }
