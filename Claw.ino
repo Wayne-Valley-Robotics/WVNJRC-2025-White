094bbd8f294f946a5226a972e95267af5bec4d78
Kiefte454 <2028kieftewv@gmail.com>
2025-05-03 13:51:11 -0400

    comment added

--- Claw.ino
+++ Claw.ino
@@ -1,8 +1,17 @@
 void Claw() {
+<<<<<<< Updated upstream
   //claw open = O;
   //claw close = C;
   int S = 1;
   prizm.setServoSpeed(S, 70);
+=======
+ // I am so done with this code, but code I must
+  
+  int O = number;
+  int C = number;
+  int S = 1;
+  prizm.setServoSpeed(S, 60);
+>>>>>>> Stashed changes
 
   if (ps4.Button(L2T)) {
     if (prizm.readServoPosition(S)== (O)){
