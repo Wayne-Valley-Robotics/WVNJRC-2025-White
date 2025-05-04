e9d184b87151783bbb3697f03fece65ad360c414
aayush-pandya <ahpandya06@gmail.com>
2025-05-04 18:19:14 -0400

    replaced old code
    now dependent on how much the left trigger is pressed, not a set amount

--- Claw.ino
+++ Claw.ino
@@ -1,24 +1,17 @@
-void Claw() {
-<<<<<<< Updated upstream
-  //claw open = O;
-  //claw close = C;
-  int S = 1;
-  prizm.setServoSpeed(S, 70);
-=======
- // I am so done with this code, but code I must
-  
-  int O = number;
-  int C = number;
-  int S = 1;
-  prizm.setServoSpeed(S, 60);
->>>>>>> Stashed changes
+int servo = 1;
+int l_trigger;
 
-  if (ps4.Button(L2T)) {
-    if (prizm.readServoPosition(S)== (O)){
-    prizm.setServoPosition(S, C);
-    }
-    else if (prizm.readServoPosition(S)==(C)){
-    prizm.setServoPosition(S, O);
-    
-  }
+void Claw() {
+  prizm.setServoSpeed(servo, 70);
+  // I am so done with this code, but code I must
+  l_trigger = map(ps4.Button(L2T), 0, 255, 90, 0);
+
+  prizm.setServoPosition(servo, l_trigger);
+  // if (ps4.Button(L2T)) {
+  //   if (prizm.readServoPosition(servo)== (open_deg)){
+  //   prizm.setServoPosition(servo, close_deg);
+  //   }
+  //   else if (prizm.readServoPosition(servo)==(close_deg)){
+  //   prizm.setServoPosition(servo, open_deg);
+  // }
 }
