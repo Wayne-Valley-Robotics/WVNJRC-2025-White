3d9576c3d4c5fa97787a98bd142c44dc23aa5caf
Kiefte454 <2028kieftewv@gmail.com>
2025-05-05 08:16:50 -0400

    Fixed Gantry; need test

--- Gantry.ino
+++ Gantry.ino
@@ -3,51 +3,55 @@ void GantrySystem() {
   int motor_speed = 40; // motor speed
   int bottom_sensor = A2; // bottom
 
-  // right is right, NOT LEFT
+ // right is right, NOT LEFT
   if (ps4.Button(RIGHT)) {
     exc.setMotorPower(3, 1, motor_speed);
+    prizm.setMotorPower(1, 125);
+    prizm.setMotorPower(2, 125);
   }
-  else {
-    exc.setMotorPower(3, 1, 125);
-  }
-  
-  // left better not move anywhere except left
-  if (ps4.Button(LEFT)) {
-    exc.setMotorPower(3, 1, -motor_speed);
-  }
-  else {
-    exc.setMotorPower(3, 1, 125);
-  }
-  
-  // now the hard part, two consecutive ha ha ha :,)
 
-  /* the two vertical motors DO NOT USE EXC they use PRIZM 
+// left better not move anywhere except left
+  else if (ps4.Button(LEFT)) {
+    exc.setMotorPower(3, 1, -motor_speed);
+    prizm.setMotorPower(1, 125);
+    prizm.setMotorPower(2, 125);
+  }
+
+// now the hard part, two consecutive ha ha ha :,)
+
+/* the two vertical motors DO NOT USE EXC they use PRIZM 
   Motor 1: smaller vertical one
   Motor 2: main one (bigger) */
 
   // up up and away!!!
-  if ((ps4.Button(UP)) && (!digitalRead(top_sensor))) {  // no switch
-    prizm.setMotorPower(1, motor_speed);
-  }
-  else if ((ps4.Button(UP)) && (digitalRead(top_sensor))) { // with switch
-    prizm.setMotorPower(2, motor_speed);
-    prizm.setMotorPower(1, 125);
-  }
-  else { // stop
-    prizm.setMotorPower(1, 125);
-    prizm.setMotorPower(2, 125);
+  else if (ps4.Button(UP)) {
+    if(!digitalRead(top_sensor)){// no switch
+      exc.setMotorPower(3, 1, 125);
+      prizm.setMotorPower(1, motor_speed);
+      prizm.setMotorPower(2, 125);
+    }
+    else{//with switch
+      exc.setMotorPower(3, 1, 125);
+      prizm.setMotorPower(1, 125);
+      prizm.setMotorPower(2, motor_speed);
+    }
   }
 
-  // down time is frown time :(
-  if ((ps4.Button(DOWN)) && (!digitalRead(bottom_sensor))) {  // no switch
-    prizm.setMotorPower(1, -motor_speed);
+  else if (ps4.Button(DOWN)) {
+    if(!digitalRead(bottom_sensor)){ // no switch
+      exc.setMotorPower(3, 1, 125);
+      prizm.setMotorPower(1, -motor_speed);
+      prizm.setMotorPower(2, 125);
+    }
+    else{ //with switch
+      exc.setMotorPower(3, 1, 125);
+      prizm.setMotorPower(1, 125);
+      prizm.setMotorPower(2, -motor_speed);
+    }
   }
-  else if ((ps4.Button(DOWN)) && (digitalRead(bottom_sensor))){ //with switch
-    prizm.setMotorPower(2, -motor_speed);
-    prizm.setMotorPower(1, 125);
-  }
-  else { //stop
+
+  else { // stop
+    exc.setMotorPower(3, 1, 125);
     prizm.setMotorPower(1, 125);
     prizm.setMotorPower(2, 125);
   }
-}
