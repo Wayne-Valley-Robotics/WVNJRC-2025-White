a782a306954bd8e1a60127c448eddec693db04d2
ShannonKiefte <Kiefteshannon@gmail.com>
2025-05-05 12:08:13 -0400

    gantry code fixed and tested

--- Gantry.ino
+++ Gantry.ino
@@ -1,17 +1,15 @@
 void GantrySystem() {
-  int top_sensor = A1; // top sensor
-  int motor_speed = 40; // motor speed
-  int bottom_sensor = A2; // bottom
+  int motor_speed = 60; // motor speed
 
  // right is right, NOT LEFT
-  if (ps4.Button(RIGHT)) {
+  if (ps4.Button(LEFT)) {
     exc.setMotorPower(3, 1, motor_speed);
     prizm.setMotorPower(1, 125);
     prizm.setMotorPower(2, 125);
   }
 
 // left better not move anywhere except left
-  else if (ps4.Button(LEFT)) {
+  else if (ps4.Button(RIGHT)) {
     exc.setMotorPower(3, 1, -motor_speed);
     prizm.setMotorPower(1, 125);
     prizm.setMotorPower(2, 125);
@@ -24,34 +22,33 @@ void GantrySystem() {
   Motor 2: main one (bigger) */
 
   // up up and away!!!
-  else if (ps4.Button(UP)) {
-    if(!digitalRead(top_sensor)){// no switch
+  else if (ps4.Button(UP)) { // front up
       exc.setMotorPower(3, 1, 125);
       prizm.setMotorPower(1, motor_speed);
       prizm.setMotorPower(2, 125);
     }
-    else{//with switch
+  else if (ps4.Button(L1)){ // back up
       exc.setMotorPower(3, 1, 125);
       prizm.setMotorPower(1, 125);
       prizm.setMotorPower(2, motor_speed);
     }
-  }
+  
 
-  else if (ps4.Button(DOWN)) {
-    if(!digitalRead(bottom_sensor)){ // no switch
+  else if (ps4.Button(DOWN)) { // front down
       exc.setMotorPower(3, 1, 125);
       prizm.setMotorPower(1, -motor_speed);
       prizm.setMotorPower(2, 125);
     }
-    else{ //with switch
+  else if (ps4.Button(R1)) { // back down
       exc.setMotorPower(3, 1, 125);
       prizm.setMotorPower(1, 125);
       prizm.setMotorPower(2, -motor_speed);
     }
-  }
+  
 
   else { // stop
     exc.setMotorPower(3, 1, 125);
     prizm.setMotorPower(1, 125);
     prizm.setMotorPower(2, 125);
   }
+}
